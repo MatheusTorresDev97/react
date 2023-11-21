ada0f7cb1a94a59675fc1c3125e84d04d2db204a
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-21 09:34:14 -0300

    Introducao as Rotas

---
+++ 03-routes/01-router/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Sobre from "./components/Sobre";
+import Contato from "./components/Contato";
+import Pagina404 from "./components/Pagina404";
+
+//React Router Dom
+//É uma extensão que permite gerenciarmos as rotas do React.
+//npm install react-router-dom
+
+//BrowserRouter, Routes e Route
+//O BrowserRouter deve ser o componente pai que envolve tudo que depender do react-router.
+//O Routes define a área em que vamos colocar os nossos Route
+//O Route recebe um caminho em path, se esse caminho for o mesmo do URL ele irá renderizar o component que estiver dentro de element={}.
+
+//404 - Não Encontrado
+//O * renderiza um elemento para todas as rotas que não foram definidas em path. Uso ideal para mostrarmos um componente indicando que a página não existe.
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sobre" element={<Sobre />} />
+        <Route path="contato" element={<Contato />} />
+        <Route path="*" element={<Pagina404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
