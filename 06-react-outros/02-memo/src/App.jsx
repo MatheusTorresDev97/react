e0498f82327eea4d072766041b4811a05aaf3213
Matheus <matheustorresdev@gmail.com>
2023-11-21 20:15:32 -0300

    memoizacao

---
+++ 06-react-outros/02-memo/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+
+//Retorna um componente memorizado, evitando que o mesmo seja atualizado toda vez que o estado de um elemento pai mudar. Use apenas para elementos que não dependam de estados diferentes.
+
+const App = () => {
+  const [contar, setContar] = React.useState(0);
+  return (
+    <div>
+      <Header />
+      <button onClick={() => setContar(contar + 1)}>{contar}</button>
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ 06-react-outros/02-memo/src/Header.jsx
@@ -0,0 +1,10 @@
+import React, { memo } from 'react'
+
+const Header = () => {
+
+    console.log('Renderizou')
+    return <div>Header fixo</div>
+
+}
+
+export default memo(Header);
