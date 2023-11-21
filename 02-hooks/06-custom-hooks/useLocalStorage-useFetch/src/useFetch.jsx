119c432467127c8a0686e6a36a03c20d9a83d397
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-21 09:29:31 -0300

    hooks customizados

---
+++ 02-hooks/06-custom-hooks/useLocalStorage-useFetch/src/useFetch.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const useFetch = () => {
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(null)
+
+    const request = async (url, options) => {
+        let response;
+        let json;
+        try {
+            setError(null)
+            setLoading(true)
+            response = await fetch(url, options);
+            json = await response.json();
+        } catch (erro) {
+            json = null
+            setError(erro)
+        } finally {
+            setData(json)
+            setLoading(false)
+            return {
+                response, json
+            }
+        }
+    }
+
+    return {
+        data,
+        error,
+        loading,
+        request
+    }
+}
+
+export default useFetch
